e0c5dc2a8cd5f595cfab3324a0d4d34ecfc178be
Reisonic <Reisonic@github.com>
2021-04-02 10:59:28 +0300

    Initial commit

---
+++ wi-fiscanner/src/main/java/lo/rd/wi_fiscanner/MainActivity.java
@@ -0,0 +1,271 @@
+package lo.rd.wi_fiscanner;
+
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.List;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.Manifest.permission.ACCESS_NETWORK_STATE;
+import static android.Manifest.permission.ACCESS_WIFI_STATE;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "AndroidExample";
+
+    private static final int MY_REQUEST_CODE = 123;
+
+    private WifiManager wifiManager;
+
+    private EditText editTextPassword;
+    private LinearLayout linearLayoutScanResults;
+    private TextView textViewScanResults;
+
+    private WifiBroadcastReceiver wifiReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        // Instantiate broadcast receiver
+        wifiReceiver = new WifiBroadcastReceiver();
+
+        // Register the receiver
+        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+
+        //
+        Button buttonState = findViewById(R.id.button_state);
+        Button buttonScan = findViewById(R.id.button_scan);
+
+        editTextPassword = findViewById(R.id.editText_password);
+        textViewScanResults = findViewById(R.id.textView_scanResults);
+        linearLayoutScanResults = findViewById(R.id.linearLayout_scanResults);
+
+        buttonState.setOnClickListener(view -> showWifiState());
+
+        buttonScan.setOnClickListener(view -> askAndStartScanWifi());
+    }
+
+
+    private void askAndStartScanWifi() {
+
+        // With Android Level >= 23, you have to ask the user
+        // for permission to Call.
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) { // 23
+            int permission1 = ContextCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION);
+
+            // Check for permissions
+            if (permission1 != PackageManager.PERMISSION_GRANTED) {
+
+                Log.d(LOG_TAG, "Requesting Permissions");
+
+                // Request permissions
+                ActivityCompat.requestPermissions(this,
+                        new String[]{
+                                ACCESS_COARSE_LOCATION,
+                                ACCESS_FINE_LOCATION,
+                                ACCESS_WIFI_STATE,
+                                ACCESS_NETWORK_STATE
+                        }, MY_REQUEST_CODE);
+                return;
+            }
+            Log.d(LOG_TAG, "Permissions Already Granted");
+        }
+        doStartScanWifi();
+    }
+
+    private void doStartScanWifi() {
+        wifiManager.startScan();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(LOG_TAG, "onRequestPermissionsResult");
+
+        if (requestCode == MY_REQUEST_CODE) {// If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // permission was granted
+                Log.d(LOG_TAG, "Permission Granted: " + permissions[0]);
+
+                // Start Scan Wifi.
+                this.doStartScanWifi();
+            } else {
+                // Permission denied, boo! Disable the
+                // functionality that depends on this permission.
+                Log.d(LOG_TAG, "Permission Denied: " + permissions[0]);
+            }
+            // Other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
+    private void showWifiState() {
+        int state = this.wifiManager.getWifiState();
+        String statusInfo;
+
+        switch (state) {
+            case WifiManager.WIFI_STATE_DISABLING:
+                statusInfo = "Disabling";
+                break;
+            case WifiManager.WIFI_STATE_DISABLED:
+                statusInfo = "Disabled";
+                break;
+            case WifiManager.WIFI_STATE_ENABLING:
+                statusInfo = "Enabling";
+                break;
+            case WifiManager.WIFI_STATE_ENABLED:
+                statusInfo = "Enabled";
+                break;
+            default:
+                statusInfo = "Unknown";
+                break;
+        }
+        Toast.makeText(this, "Wifi Status: " + statusInfo, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStop() {
+        this.unregisterReceiver(this.wifiReceiver);
+        super.onStop();
+    }
+
+    private void showNetworks(List<ScanResult> results) {
+        this.linearLayoutScanResults.removeAllViews();
+
+        for (final ScanResult result : results) {
+            final String networkCapabilities = result.capabilities;
+            final String networkSSID = result.SSID; // Network Name.
+            //
+            Button button = new Button(this);
+
+            button.setText(String.format("%s (%s)", networkSSID, networkCapabilities));
+            this.linearLayoutScanResults.addView(button);
+
+            button.setOnClickListener(view -> {
+                String networkCapabilities1 = result.capabilities;
+                connectToNetwork(networkCapabilities1, networkSSID);
+            });
+        }
+    }
+
+    private void showNetworksDetails(List<ScanResult> results) {
+
+        this.textViewScanResults.setText("");
+        StringBuilder sb = new StringBuilder();
+        sb.append("Result Count: ").append(results.size());
+
+        for (int i = 0; i < results.size(); i++) {
+            ScanResult result = results.get(i);
+            sb.append("\n\n  --------- Network ").append(i).append("/").append(results.size()).append(" ---------");
+
+            sb.append("\n result.capabilities: ").append(result.capabilities);
+            sb.append("\n result.SSID: ").append(result.SSID); // Network Name.
+
+            sb.append("\n result.BSSID: ").append(result.BSSID);
+            sb.append("\n result.frequency: ").append(result.frequency);
+            sb.append("\n result.level: ").append(result.level);
+
+            sb.append("\n result.describeContents(): ").append(result.describeContents());
+
+            // Level >17, Android 4.2
+            sb.append("\n result.timestamp: ").append(result.timestamp);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { // Level 23, Android 6.0
+                sb.append("\n result.centerFreq0: ").append(result.centerFreq0);
+                sb.append("\n result.centerFreq1: ").append(result.centerFreq1);
+                sb.append("\n result.venueName: ").append(result.venueName);
+                sb.append("\n result.operatorFriendlyName: ").append(result.operatorFriendlyName);
+                sb.append("\n result.channelWidth: ").append(result.channelWidth);
+                sb.append("\n result.is80211mcResponder(): ").append(result.is80211mcResponder());
+                sb.append("\n result.isPasspointNetwork(): ").append(result.isPasspointNetwork());
+            }
+        }
+        this.textViewScanResults.setText(sb.toString());
+    }
+
+    private void connectToNetwork(String networkCapabilities, String networkSSID) {
+        Toast.makeText(this, "Connecting to network: " + networkSSID, Toast.LENGTH_SHORT).show();
+
+        String networkPass = this.editTextPassword.getText().toString();
+
+        WifiConfiguration wifiConfig = new WifiConfiguration();
+        wifiConfig.SSID = "\"" + networkSSID + "\"";
+
+        if (networkCapabilities.toUpperCase().contains("WEP")) { // WEP Network.
+            Toast.makeText(this, "WEP Network", Toast.LENGTH_SHORT).show();
+
+            wifiConfig.wepKeys[0] = "\"" + networkPass + "\"";
+            wifiConfig.wepTxKeyIndex = 0;
+            wifiConfig.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+            wifiConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
+        } else if (networkCapabilities.toUpperCase().contains("WPA")) { // WPA Network
+            Toast.makeText(this, "WPA Network", Toast.LENGTH_SHORT).show();
+            wifiConfig.preSharedKey = "\"" + networkPass + "\"";
+        } else { // OPEN Network.
+            Toast.makeText(this, "OPEN Network", Toast.LENGTH_SHORT).show();
+            wifiConfig.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+        }
+
+        this.wifiManager.addNetwork(wifiConfig);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        List<WifiConfiguration> list = wifiManager.getConfiguredNetworks();
+        for (WifiConfiguration config : list) {
+            if (config.SSID != null && config.SSID.equals("\"" + networkSSID + "\"")) {
+                wifiManager.disconnect();
+                wifiManager.enableNetwork(config.networkId, true);
+                wifiManager.reconnect();
+                break;
+            }
+        }
+    }
+
+    // Define class to listen to broadcasts
+    class WifiBroadcastReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(LOG_TAG, "onReceive()");
+
+            Toast.makeText(MainActivity.this, "Scan Complete!", Toast.LENGTH_SHORT).show();
+
+            boolean ok = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false);
+
+            if (ok) {
+                Log.d(LOG_TAG, "Scan OK");
+
+                List<ScanResult> list = wifiManager.getScanResults();
+
+                MainActivity.this.showNetworks(list);
+                MainActivity.this.showNetworksDetails(list);
+            } else {
+                Log.d(LOG_TAG, "Scan not OK");
+            }
+        }
+    }
+}
