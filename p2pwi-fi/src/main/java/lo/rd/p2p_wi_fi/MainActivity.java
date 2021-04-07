d9f54a158525771965e4dfe4f30dfc16d04f555f
dan_work_KamAZ <bessonovdanil9112@mail.ru>
2021-04-07 10:12:50 +0300

    P2P wi-fi
    init commit

---
+++ p2pwi-fi/src/main/java/lo/rd/p2p_wi_fi/MainActivity.java
@@ -0,0 +1,14 @@
+package lo.rd.p2p_wi_fi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
